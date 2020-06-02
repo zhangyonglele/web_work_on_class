3fd0dd005357c4cd3713284ec7e9320ffa384dcb
zhangyonglele <zhangyonglele@yeah.net>
2020-06-02 11:37:46 +0800

    修改项目建议书

--- src/main/java/com/work/web_work_on_class/service/impl/ProjectGroupServiceImpl.java
+++ src/main/java/com/work/web_work_on_class/service/impl/ProjectGroupServiceImpl.java
@@ -1,6 +1,18 @@
 package com.work.web_work_on_class.service.impl;
 
+import com.work.web_work_on_class.model.ProjectGroup;
 import com.work.web_work_on_class.service.ProjectGroupService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ProjectGroupServiceImpl implements ProjectGroupService {
+    @Override
+    public boolean addNewGroup(ProjectGroup record) {
+        return false;
+    }
+
+    @Override
+    public ProjectGroup getGroupInfo(int groupId) {
+        return null;
+    }
 }
