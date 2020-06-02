ca3a82a1449bbfa0cc8ab8c0306f48101f715bd4
zhangyonglele <zhangyonglele@yeah.net>
2020-06-03 00:49:11 +0800

    修改项目建议书

--- src/main/java/com/work/web_work_on_class/WebWorkOnClassApplication.java
+++ src/main/java/com/work/web_work_on_class/WebWorkOnClassApplication.java
@@ -4,8 +4,11 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.session.config.annotation.web.http.EnableSpringHttpSession;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.session.web.http.CookieSerializer;
+import org.springframework.session.web.http.DefaultCookieSerializer;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
@@ -19,4 +22,13 @@ public class WebWorkOnClassApplication {
         SpringApplication.run(WebWorkOnClassApplication.class, args);
     }
 
+    @Bean
+    public CookieSerializer httpSessionIdResolver() {
+        DefaultCookieSerializer cookieSerializer = new DefaultCookieSerializer();
+        //cookieSerializer.setCookieName("token");
+        cookieSerializer.setUseHttpOnlyCookie(false);
+        cookieSerializer.setSameSite("None");
+        cookieSerializer.setUseSecureCookie(true);
+        return cookieSerializer;
+    }
 }
